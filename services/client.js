f4653eb72e6b5ecead1065bb306bbd084557f997
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2026-08-25 18:59:04 +0600

    admin controll done

--- services/client.js
+++ services/client.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'https://localhost:5000/'; // 🔧 Change to your backend URL
+const BASE_URL = 'https://localhost:5000/';
 
 class ApiClient {
   constructor() {
